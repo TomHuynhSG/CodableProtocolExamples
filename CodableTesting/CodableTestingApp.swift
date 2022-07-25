789cb906706ae62f93fd98bc6fa7abbb3eed4677
Tom Huynh <tomhuynhsg@gmail.com>
2022-07-25 12:13:43 +0700

    Initial Commit

---
+++ CodableTesting/CodableTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodableTestingApp.swift
+//  CodableTesting
+//
+//  Created by Tom Huynh on 7/25/22.
+//
+
+import SwiftUI
+
+@main
+struct CodableTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
